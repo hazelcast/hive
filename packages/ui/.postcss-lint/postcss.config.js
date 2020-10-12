90285dfadbff894ee7e09b82ac0c40450a8513fc
Andrey Goncharov <andrey@goncharov.page>
2020-10-12 18:19:25 +0300

    Add SCSS linting

---
+++ packages/ui/.postcss-lint/postcss.config.js
@@ -0,0 +1,34 @@
+// eslint-disable-next-line import/no-extraneous-dependencies
+const doiuse = require('doiuse')
+
+const browsers = ['last 2 Chrome version', 'last 2 Firefox version', 'last 1 Safari version', 'last 1 Edge version', 'ie 11']
+
+/*
+ * EXPERIMENTAL: This postcss config is and should be only used
+ * as a trivial audit of "doiuse" compatibility rules
+ *
+ * Postcss-cli does not allow config argument override, what it does
+ * allow is specifying an alternate config path, hence this location
+ */
+module.exports = {
+  syntax: 'postcss-scss',
+  plugins: [
+    // eslint-disable-next-line
+    require('stylelint'),
+    doiuse({
+      browsers,
+      //These problems are taken case of by 'autoprefixer'
+      ignore: [
+        // Autoprefixer takes care of flex
+        'flexbox',
+        // The problem is "outline-offset", which doiuse can't detect
+        'outline',
+        // The problem is "vmax", which doiuse can' detect
+        'viewport-units',
+      ],
+      ignoreFiles: ['**/node_modules/**/*.css', '**/node_modules/**/*.scss', '**/coverage/**/*'],
+    }),
+    // eslint-disable-next-line
+    require('postcss-reporter')({ throwError: true }),
+  ],
+}
---
+++ packages/ui/stylelint.config.js
@@ -0,0 +1,42 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-prettier'],
+  plugins: [
+    // SCSS rules set, need to be explicitly set in "rules"
+    'stylelint-scss',
+  ],
+  rules: {
+    /*
+     * CSS-modules rules (from stylelint-config-css-module)
+     * Overrides of stylelint-config-standard for CSS modules
+     */
+    'selector-pseudo-class-no-unknown': [
+      true,
+      {
+        ignorePseudoClasses: ['export', 'import', 'global', 'local', 'external'],
+      },
+    ],
+    'selector-type-no-unknown': [
+      true,
+      {
+        ignoreTypes: ['from'],
+      },
+    ],
+    'property-no-unknown': [
+      true,
+      {
+        ignoreProperties: ['composes', 'compose-with'],
+        ignoreSelectors: [':export', /^:import/],
+      },
+    ],
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['value', 'extend', 'mixin', 'include'],
+      },
+    ],
+    // stylelint-scss rules
+    'scss/no-duplicate-dollar-variables': true,
+    'scss/dollar-variable-colon-space-after': 'always-single-line',
+    'comment-empty-line-before': null,
+  },
+}
