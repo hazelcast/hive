8a98ba7d398375a9b8a781e6ad941064b4b4471d
Jan Koritak <koritakjan@gmail.com>
2020-11-04 17:06:15 +0100

    feat: add number badge

---
+++ packages/ui/__stories__/NumberBadge.stories.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+import { NumberBadge } from '../src/NumberBadge'
+
+export default {
+  title: 'components/NumberBadge',
+  component: NumberBadge,
+}
+
+export const Default = () => <NumberBadge number={1} />
+
+export const LessThan99 = () => <NumberBadge number={99} />
+
+export const MoreThan99 = () => <NumberBadge number={100} />
