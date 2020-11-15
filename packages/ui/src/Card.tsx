4a481f3c48dd6bbff9de84d4fd2debfd2e097b0b
Jozef <jo.hruska27@gmail.com>
2020-11-15 11:48:45 +0100

    refactor(ui/card): apply suggestions from PR review

--- packages/ui/src/Card.tsx
+++ packages/ui/src/Card.tsx
@@ -1,5 +1,6 @@
 import React, { FC } from 'react'
 import { Icon, IconProps } from './Icon'
+import { DataTestProp } from '@hazelcast/helpers'
 import cn from 'classnames'
 
 import { IconButton, IconButtonProps } from './IconButton'
@@ -27,7 +28,8 @@ type CardHeaderProps =
 export type CardProps = {
   type?: 'primary' | 'secondary' | 'highlighter'
   icon?: IconProps['icon']
-} & CardHeaderProps
+} & CardHeaderProps &
+  DataTestProp
 
 /**
  * ### Purpose
@@ -38,42 +40,44 @@ export type CardProps = {
  * - "Secondary" and "Highlighter" Cards should be used only within "Primary" Cards.
  * - Card can display an icon next to Card heading. This icon can be static or interactive (button).
  */
-export const Card: FC<CardProps> = ({ type = 'primary', title, icon, iconButton, children }) => {
-  /**
-   * Helper method to render plain or interactive icon (IconButton).
-   */
-let icon: JSX.Element = null
-if (icon !== undefined) {
-  icon = <Icon icon={icon} color={styleConsts.colorPrimary} size={type === 'primary' ? 'normal' : 'small'} ariaHidden />
-}
-if (iconButton !== undefined) {
-   icon = (
-        <IconButton kind="transparent" color={styleConsts.colorPrimary} size={type === 'primary' ? 'normal' : 'small'} {...iconButton} />
-   )
-}
+export const Card: FC<CardProps> = ({ type = 'primary', title, icon, iconButton, 'data-test': dataTest, children }) => {
+  let iconElement: JSX.Element | null = null
+
+  if (icon !== undefined) {
+    iconElement = <Icon icon={icon} color={styleConsts.colorPrimary} size={type === 'primary' ? 'normal' : 'small'} ariaHidden />
+  }
+
+  if (iconButton !== undefined) {
+    iconElement = (
+      <IconButton kind="transparent" color={styleConsts.colorPrimary} size={type === 'primary' ? 'normal' : 'small'} {...iconButton} />
+    )
+  }
 
   return (
     <div
-      data-test="card-wrapper"
+      data-test={dataTest ?? 'card-wrapper'}
       className={cn(styles.wrapper, {
         [styles.typeSecondary]: type === 'secondary',
         [styles.typeHighlighter]: type === 'highlighter',
       })}
     >
       {title && (
+        <>
           {type === 'primary' ? (
-            <h2 data-test="card-heading" className={styles.header}>
+            <h2 data-test="card-heading" className={styles.heading}>
               {title}
-              {renderIcon()}
+              <span className={styles.icon}>{iconElement}</span>
             </h2>
           ) : (
-            <h3 data-test="card-heading" className={styles.header}>
+            <h3 data-test="card-heading" className={styles.heading}>
               {title}
-              {renderIcon()}
+              <span className={styles.icon}>{iconElement}</span>
             </h3>
           )}
+        </>
       )}
-      <div data-test="card-content" className={cn(styles.content, { [styles.woTitle]: !title })}>
+
+      <div data-test="card-content" className={cn(styles.content)}>
         {children}
       </div>
     </div>
