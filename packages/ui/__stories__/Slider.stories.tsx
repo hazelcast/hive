0c94b104491266179efe9a05f43ab9d425bb87c6
Jan Vorcak <vorcak@webscope.io>
2020-11-16 12:51:44 +0100

    Added some corner-case stories

--- packages/ui/__stories__/Slider.stories.tsx
+++ packages/ui/__stories__/Slider.stories.tsx
@@ -1,5 +1,8 @@
 import React, { useState } from 'react'
 import { Slider } from '../src/Slider'
+import styles from '../src/Slider.module.scss'
+import cn from 'classnames'
+import utilStyles from './utils.scss'
 
 export default {
   title: 'Components/Slider',
@@ -17,6 +20,21 @@ export const DefaultSlider = () => {
   )
 }
 
+export const HoveredSlider = () => {
+  const [value, onChange] = useState<number>(40)
+  return <Slider value={value} onChange={onChange} min={0} max={100} sliderClassName={styles.hover} />
+}
+
+export const FocusedSlider = () => {
+  const [value, onChange] = useState<number>(40)
+  return <Slider value={value} onChange={onChange} min={0} max={100} sliderClassName={styles.focus} />
+}
+
+export const HoveredFocusedSlider = () => {
+  const [value, onChange] = useState<number>(40)
+  return <Slider value={value} onChange={onChange} min={0} max={100} sliderClassName={cn(styles.focus, styles.hover)} />
+}
+
 export const SliderWithRange = () => {
   const [value, setValue] = useState<[number, number]>([0, 10])
   return (
@@ -28,4 +46,40 @@ export const SliderWithRange = () => {
       <Slider value={value} onChange={setValue} min={0} max={10} />
     </>
   )
+}
+
+export const SliderWithError = () => {
+  const [value, onChange] = useState<number>(0)
+  return (
+    <div>
+      <Slider value={value} onChange={onChange} min={0} max={100} error="This value is invalid" />
+    </div>
+  )
+}
+
+export const SliderWithHelperText = () => {
+  const [value, onChange] = useState<number>(0)
+  return (
+    <div>
+      <Slider value={value} onChange={onChange} min={0} max={100} helperText="Helper text" />
+    </div>
+  )
+}
+
+/**
+ * The following stories are here mostly mostly for visual regression testing and do not demonstrate the way
+ * how this component should be used.
+ * @constructor
+ */
+export const DoNotUse__OpacityThumb__Beginning = () => {
+  const [value, onChange] = useState<number>(0)
+  return <Slider value={value} onChange={onChange} min={0} max={100} sliderClassName={utilStyles.sliderSmallThumb} />
+}
+export const DoNotUse__OpacityThumb__Middle = () => {
+  const [value, onChange] = useState<number>(50)
+  return <Slider value={value} onChange={onChange} min={0} max={100} sliderClassName={utilStyles.sliderSmallThumb} />
+}
+export const DoNotUse__OpacityThumb__End = () => {
+  const [value, onChange] = useState<number>(100)
+  return <Slider value={value} onChange={onChange} min={0} max={100} sliderClassName={utilStyles.sliderSmallThumb} />
 }
--- packages/ui/src/Slider.tsx
+++ packages/ui/src/Slider.tsx
@@ -1,54 +1,84 @@
-import React, { ChangeEvent, useCallback, useEffect, useRef, useState } from 'react'
+import React, { ChangeEvent, useCallback, useEffect, useRef } from 'react'
 import styles from './Slider.module.scss'
 import cn from 'classnames'
+import { Error } from './Error'
+import { v4 as uuid } from 'uuid'
+import { Help } from './Help'
 
-type SliderPropsCore = {
+type Value = number | [number, number]
+
+type SliderProps<T> = {
   step?: number
   min?: number
   max?: number
+  value: T
+  onChange: (val: T) => void
+  className?: string
+  sliderClassName?: string
+  error?: string
+  helperText?: string
 }
 
-type SliderPropsSingle = {
-  value: number
-  onChange: (value: number) => void
-}
-
-type SliderPropsRange = {
-  value: [number, number]
-  onChange: (value: [number, number]) => void
-}
-
-type SliderProps = SliderPropsCore & (SliderPropsSingle | SliderPropsRange)
-
-function isRangeGuard(value: number | [number, number]): value is [number, number] {
+function isRangeGuard(value: Value): value is [number, number] {
   return Array.isArray(value)
 }
 
-export const Slider = ({ value, onChange, step = 1, min = 0, max = 20 }: SliderProps) => {
-  const isRange: boolean = isRangeGuard(value)
-  const wrapperRef = useRef<HTMLDivElement>(null)
-  const didMountRef = useRef(false)
+export function Slider<T extends Value = number>({
+  value,
+  onChange,
+  step = 1,
+  min = 0,
+  max = 20,
+  className,
+  sliderClassName,
+  error,
+  helperText,
+}: SliderProps<T>) {
+  const idRef = useRef(uuid())
 
-  const [firstValue, setFirstValue] = useState<number>(isRangeGuard(value) ? value[0] : value)
-  const [secondValue, setSecondValue] = useState<number>(isRangeGuard(value) ? value[1] : max)
+  // Slider operates in two modes, either we have a single value slider or range value slider
+  const isRange: boolean = isRangeGuard(value)
+  // wrapper ref is used to catch clicks on a track in order to move the closes thumb to the position
+  const wrapperRef = useRef<HTMLDivElement>(null)
+
+  const [firstValue, secondValue]: [number, number] = isRangeGuard(value) ? value : [value as number, max]
 
   const firstRef = useRef<HTMLInputElement>(null)
   const secondRef = useRef<HTMLInputElement>(null)
 
+  const updateValues = useCallback(
+    ([changedFirstValue, changedSecondValue]: [number | null, number | null]) => {
+      if (isRange) {
+        onChange([changedFirstValue ?? firstValue, changedSecondValue ?? secondValue] as T)
+      } else {
+        onChange(changedFirstValue as T)
+      }
+    },
+    [isRange, firstValue, secondValue],
+  )
+
+  /**
+   * The following code makes sure that the relevant thumb is moved to the position once
+   * user clicks on a track.
+   *
+   * 1. We always move the first thumb in a non-range mode.
+   * 2. Otherwise, we move the thumb that is closed to a click event.
+   * 3. We focus the thumb after it is clicked.
+   */
   useEffect(() => {
     const handler = ({ offsetX, target }: MouseEvent) => {
-      if (wrapperRef?.current?.offsetWidth && wrapperRef.current === target) {
+      if (wrapperRef.current && wrapperRef.current === target) {
         const clickPercentage = offsetX / wrapperRef.current.offsetWidth
         const resultStepSpot: number = Math.round((clickPercentage * max) / step) * step
 
+        // let's move the first thumb in case we're not in a range mode
+        // or if it's closer to the clicked value
         if (!isRange || Math.abs(firstValue - resultStepSpot) < Math.abs(secondValue - resultStepSpot)) {
-          // let's move the first thumb in case we're not in a range more
-          // or if it's closed to the click event
-          setFirstValue(resultStepSpot)
+          updateValues([resultStepSpot, null])
           firstRef.current?.focus()
         } else {
           // otherwise, let's move the right one
-          setSecondValue(resultStepSpot)
+          updateValues([null, resultStepSpot])
           secondRef.current?.focus()
         }
       }
@@ -60,100 +90,90 @@ export const Slider = ({ value, onChange, step = 1, min = 0, max = 20 }: SliderP
     }
   }, [wrapperRef, firstValue, secondValue])
 
-  useEffect(() => {
-    if (didMountRef.current) {
-      if (isRangeGuard(value)) {
-        onChange([firstValue, secondValue])
-      } else {
-        onChange(firstValue)
-      }
-    } else {
-      // let's ignore the first render
-      didMountRef.current = true
-    }
-  }, [firstValue, secondValue])
-
-  const setFirstValueFn = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      const {
-        target: { value },
-      } = e
-
+  /**
+   * The following callbacks reacts to native HTML events and update the values
+   */
+  const setFirstValueCallback = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
       if (+value > secondValue) {
         return false
       }
-      setFirstValue(+value)
+      updateValues([+value, null])
     },
     [secondValue],
   )
 
-  const setSecondValueFn = useCallback(
-    (e: ChangeEvent<HTMLInputElement>) => {
-      const {
-        target: { value },
-      } = e
-
+  const setSecondValueCallback = useCallback(
+    ({ target: { value } }: ChangeEvent<HTMLInputElement>) => {
       if (+value < firstValue) {
         return false
       }
-      setSecondValue(+value)
+      updateValues([null, +value])
     },
     [firstValue],
   )
 
-  const width = ((secondValue - firstValue) / max) * 100
-  const left = firstValue / max
+  const width: number = ((secondValue - firstValue) / max) * 100
+  const left: number = firstValue / max
 
   return (
-    <div className={styles.wrapper} role="group" ref={wrapperRef}>
-      <div className={styles.fillPlaceholder}>
-        <div
-          className={styles.fill}
-          // Since there is no easy way hot to pass properties to SCSS, we need to use
-          // inline styles.
-          style={
-            isRange
-              ? {
-                  width: `${width}%`,
-                  marginLeft: `calc(${left * 100}%)`,
-                }
-              : {
-                  width: `calc(${max * left}%)`,
-                }
-          }
-        />
-      </div>
+    <div className={className}>
+      <div className={styles.wrapper}>
+        <div className={styles.groupWrapper} role="group" ref={wrapperRef}>
+          <div className={styles.fillPlaceholder}>
+            <div
+              className={styles.fill}
+              // Since there is no easy way hot to pass properties to SCSS, we need to use
+              // inline styles.
+              style={
+                isRange
+                  ? {
+                      width: `${width}%`,
+                      marginLeft: `calc(${left * 100}%)`,
+                    }
+                  : {
+                      width: `calc(${max * left}%)`,
+                    }
+              }
+            />
+          </div>
 
-      {firstValue !== undefined && (
-        <input
-          ref={firstRef}
-          type="range"
-          value={firstValue}
-          min={min}
-          max={max}
-          onChange={setFirstValueFn}
-          className={styles.slider}
-          step={step}
-        />
-      )}
-      {isRange && secondValue !== undefined && (
-        <input
-          ref={secondRef}
-          type="range"
-          value={secondValue}
-          min={min}
-          max={max}
-          onChange={setSecondValueFn}
-          step={step}
-          className={cn({
-            // The second thumb is above the first thumb in DOM.
-            // There is a corner case where both thumbs are above each other at the end
-            // of the range. In that case we need to pass pointer-events to the first thumb
-            // because that is the only one that can be moved.
-            [styles.atTheBorder]: max === secondValue && secondValue === firstValue,
-          })}
-        />
-      )}
+          {firstValue !== undefined && (
+            <input
+              ref={firstRef}
+              type="range"
+              value={firstValue}
+              min={min}
+              max={max}
+              onChange={setFirstValueCallback}
+              className={sliderClassName}
+              step={step}
+            />
+          )}
+
+          {isRange && secondValue !== undefined && (
+            <input
+              ref={secondRef}
+              type="range"
+              value={secondValue}
+              min={min}
+              max={max}
+              onChange={setSecondValueCallback}
+              step={step}
+              className={cn({
+                sliderClassName,
+                // The second thumb is above the first thumb in DOM.
+                // There is a corner case where both thumbs are above each other at the end
+                // of the range. In that case we need to pass pointer-events to the first thumb
+                // because that is the only one that can be moved.
+                [styles.atTheBorder]: max === secondValue && secondValue === firstValue,
+              })}
+            />
+          )}
+        </div>
+        {helperText && <Help parentId={idRef.current} helperText={helperText} className={styles.helperText} />}
+      </div>
+      <Error error={error} className={styles.errorContainer} inputId={idRef.current} />
     </div>
   )
 }
