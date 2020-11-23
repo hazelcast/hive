c12ef0ce4f1233a957ebaf9804ff1cacaf039b69
Jan Vorcak <vorcak@webscope.io>
2020-11-23 16:31:04 +0100

    export.scss -> export.module.scss

--- packages/ui/src/Icon.tsx
+++ packages/ui/src/Icon.tsx
@@ -1,7 +1,7 @@
 import React, { FC, SVGProps } from 'react'
 import { Icon as IconType } from 'react-feather'
 
-import styleConsts from '../styles/constants/export.scss'
+import styleConsts from '../styles/constants/export.module.scss'
 
 type IconSize = 'small' | 'normal'
 
