bd444d37b8fcada623032565340e09b688651a35
utuk <m.utku.k@gmail.com>
2020-12-17 16:35:10 +0300

    init useCloseByEscKey hook. it works. gotta check edge cases.

--- packages/ui/src/Toast.tsx
+++ packages/ui/src/Toast.tsx
@@ -2,6 +2,7 @@ import React, { FC, ReactNode } from 'react'
 import cn from 'classnames'
 import { AlertTriangle, CheckCircle, AlertCircle, Info, Icon as IconType, X } from 'react-feather'
 
+import { useCloseByEscKey } from './utils/hooks'
 import { IconButton } from './IconButton'
 import { Icon } from './Icon'
 
@@ -52,6 +53,8 @@ export type ToastProps = {
  */
 export const Toast: FC<ToastProps> = ({ type, content, closeToast, className }) => {
   const { icon, ariaLabel } = ToastIcon[type]
+
+  useCloseByEscKey((event: any) => closeToast(event))
 
   return (
     <div
