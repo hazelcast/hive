4dc9deaf66e1a1ed9130d689174b7095c1c51352
utku <m.utku.k@gmail.com>
2020-12-28 17:59:39 +0300

    use useKey underneath.

--- packages/ui/src/utils/hooks/useCloseByEscKey.ts
+++ packages/ui/src/utils/hooks/useCloseByEscKey.ts
@@ -1,4 +1,4 @@
-import { useEffect } from 'react'
+import { useKey } from 'react-use'
 
 type CallbackFunction = (e?: KeyboardEvent) => void
 
@@ -13,30 +13,20 @@ function _removeFromStack(cb: CallbackFunction) {
 }
 
 export const useCloseByEscKey = (cb: CallbackFunction) => {
-  useEffect(() => {
-    function handleKeyDown(nativeEvent: KeyboardEvent) {
-      // 'Esc' -> IE/Edge specific value
-      if (nativeEvent.key === 'Escape' || nativeEvent.key === 'Esc') {
-        // check if the last callback on the stack is this cb.
-        const lastCb = _cbStack.length ? _cbStack[_cbStack.length - 1] : null
-        if (cb && lastCb === cb) {
-          // if so, invoke it and remove it from the stack.
-          _removeFromStack(cb)
-          cb(nativeEvent)
-        }
-      }
-    }
-
-    // register this callback
-    _cbStack.push(cb)
-    document.addEventListener('keydown', handleKeyDown)
-
-    return () => {
-      document.removeEventListener('keydown', handleKeyDown)
-      // unregister this callback
+  // 'Esc' -> IE/Edge specific value
+  const predicate = (event: KeyboardEvent) => event.key === 'Escape' || event.key === 'Esc'
+  useKey(predicate, (nativeEvent: KeyboardEvent) => {
+    // check if the last callback on the stack is this cb.
+    const lastCb = _cbStack.length ? _cbStack[_cbStack.length - 1] : null
+    if (cb && lastCb === cb) {
+      // if so, invoke it and remove it from the stack.
       _removeFromStack(cb)
+      cb(nativeEvent)
     }
-  }, [cb])
+  })
+
+  // register this callback
+  _cbStack.push(cb)
 
   return null
 }
