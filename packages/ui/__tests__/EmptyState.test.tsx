33039d643f62c8c89dee8143277ac255e1c45915
Jan Koritak <koritakjan@gmail.com>
2021-01-19 17:19:43 +0100

    feat: add tests

--- packages/ui/__tests__/EmptyState.test.tsx
+++ packages/ui/__tests__/EmptyState.test.tsx
@@ -1,7 +1,136 @@
+import { mountAndCheckA11Y } from '@hazelcast/test-helpers'
+import { Figma } from '@icons-pack/react-simple-icons'
 import React from 'react'
+import { LinkRel } from '../src/Link'
+import { EmptyState } from '../src/EmptyState'
+import { ChevronRight } from 'react-feather'
+
+// Common
+const title = 'Figma'
+const description =
+  'Figma is a vector graphics editor and prototyping tool, with additional offline features enabled by desktop applications for macOS and Windows.'
+// Icon
+const icon = Figma
+const iconLabel = 'Icon Figma'
+// Action
+const action = 'Cool!'
+const actionHref = ''
+const actionTarget = '_blank'
+const actionRel: LinkRel[] = ['noopener', 'noreferrer']
 
 describe('EmptyState', () => {
-  it('Renders', () => {
-    expect(true).toBe(true)
+  it('Renders', async () => {
+    const actionOnClick = jest.fn()
+
+    const wrapper = await mountAndCheckA11Y(
+      <EmptyState title={title} icon={icon} iconLabel={iconLabel} action={action} actionOnClick={actionOnClick} />,
+    )
+
+    expect(wrapper.findDataTest('empty-state-icon').props()).toMatchObject({
+      icon,
+      ariaLabel: iconLabel,
+      size: 'large',
+    })
+    expect(wrapper.findDataTest('empty-state-title').props()).toMatchObject({
+      role: 'heading',
+      'aria-level': 3,
+    })
+    expect(wrapper.findDataTest('empty-state-title').text()).toBe(title)
+    expect(wrapper.existsDataTest('empty-state-description')).toBeFalsy()
+    expect(wrapper.existsDataTest('empty-state-link')).toBeFalsy()
+    expect(wrapper.findDataTest('empty-state-button').at(0).props()).toMatchObject({
+      onClick: actionOnClick,
+    })
+  })
+
+  it('Renders description', async () => {
+    const actionOnClick = jest.fn()
+
+    const wrapper = await mountAndCheckA11Y(
+      <EmptyState
+        title={title}
+        icon={icon}
+        description={description}
+        iconLabel={iconLabel}
+        action={action}
+        actionOnClick={actionOnClick}
+      />,
+    )
+
+    expect(wrapper.findDataTest('empty-state-icon').props()).toMatchObject({
+      icon,
+      ariaLabel: iconLabel,
+      size: 'large',
+    })
+    expect(wrapper.findDataTest('empty-state-title').props()).toMatchObject({
+      role: 'heading',
+      'aria-level': 3,
+    })
+    expect(wrapper.findDataTest('empty-state-title').text()).toBe(title)
+    expect(wrapper.findDataTest('empty-state-description').text()).toBe(description)
+    expect(wrapper.existsDataTest('empty-state-link')).toBeFalsy()
+    expect(wrapper.findDataTest('empty-state-button').at(0).props()).toMatchObject({
+      onClick: actionOnClick,
+    })
+  })
+
+  it('Renders large size', async () => {
+    const actionOnClick = jest.fn()
+
+    const wrapper = await mountAndCheckA11Y(
+      <EmptyState title={title} icon={icon} size="large" iconLabel={iconLabel} action={action} actionOnClick={actionOnClick} />,
+    )
+
+    expect(wrapper.findDataTest('empty-state-icon').props()).toMatchObject({
+      icon,
+      ariaLabel: iconLabel,
+      size: 'xlarge',
+    })
+    expect(wrapper.findDataTest('empty-state-title').props()).toMatchObject({
+      role: 'heading',
+      'aria-level': 3,
+    })
+    expect(wrapper.findDataTest('empty-state-title').text()).toBe(title)
+    expect(wrapper.existsDataTest('empty-state-description')).toBeFalsy()
+    expect(wrapper.existsDataTest('empty-state-link')).toBeFalsy()
+    expect(wrapper.findDataTest('empty-state-button').at(0).props()).toMatchObject({
+      onClick: actionOnClick,
+    })
+  })
+
+  it('Renders horizontal', async () => {
+    const wrapper = await mountAndCheckA11Y(
+      <EmptyState
+        title={title}
+        icon={icon}
+        direction="horizontal"
+        iconLabel={iconLabel}
+        action={action}
+        actionHref={actionHref}
+        actionTarget={actionTarget}
+        actionRel={actionRel}
+      />,
+    )
+
+    expect(wrapper.findDataTest('empty-state-icon').props()).toMatchObject({
+      icon,
+      ariaLabel: iconLabel,
+      size: 'large',
+    })
+    expect(wrapper.findDataTest('empty-state-title').props()).toMatchObject({
+      role: 'heading',
+      'aria-level': 3,
+    })
+    expect(wrapper.findDataTest('empty-state-title').text()).toBe(title)
+    expect(wrapper.existsDataTest('empty-state-description')).toBeFalsy()
+    expect(wrapper.findDataTest('empty-state-link').props()).toMatchObject({
+      icon: ChevronRight,
+      ariaLabel: 'Icon Chevron Right',
+      href: actionHref,
+      target: actionTarget,
+      rel: actionRel,
+      size: 'small',
+    })
+    expect(wrapper.existsDataTest('empty-state-button')).toBeFalsy()
   })
 })
--- packages/ui/src/EmptyState.tsx
+++ packages/ui/src/EmptyState.tsx
@@ -52,7 +52,7 @@ export const EmptyState: FC<EmptyStateProps> = ({
 }) => {
   const { action, actionHref, actionOnClick, actionTarget, actionRel } = restWActionProps
 
-  const iconSize = direction === 'vertical' ? (size === 'normal' ? 'large' : 'xlarge') : 'large'
+  const iconSize = direction === 'vertical' && size === 'large' ? 'xlarge' : 'large'
 
   return (
     <div
@@ -63,17 +63,23 @@ export const EmptyState: FC<EmptyStateProps> = ({
         [styles.large]: size === 'large',
       })}
     >
-      <Icon className={styles.icon} icon={icon} ariaLabel={iconLabel} size={iconSize} />
+      <Icon data-test="empty-state-icon" className={styles.icon} icon={icon} ariaLabel={iconLabel} size={iconSize} />
       <div className={styles.content}>
-        <div className={styles.title} role="heading" aria-level={3}>
+        {/* Note: div used instead of h3 due to styling override */}
+        <div data-test="empty-state-title" className={styles.title} role="heading" aria-level={3}>
           {title}
         </div>
-        {description && <div className={styles.description}>{description}</div>}
+        {description && (
+          <div data-test="empty-state-description" className={styles.description}>
+            {description}
+          </div>
+        )}
         {direction === 'horizontal' && actionHref !== undefined ? (
           <Link
+            data-test="empty-state-link"
             className={styles.action}
             icon={ChevronRight}
-            ariaLabel="Chevron right"
+            ariaLabel="Icon Chevron Right"
             href={actionHref}
             target={actionTarget}
             rel={actionRel}
@@ -82,7 +88,7 @@ export const EmptyState: FC<EmptyStateProps> = ({
             {action}
           </Link>
         ) : (
-          <Button className={styles.action} onClick={actionOnClick}>
+          <Button data-test="empty-state-button" className={styles.action} onClick={actionOnClick}>
             {action}
           </Button>
         )}
