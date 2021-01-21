fc0c7206747d2b721b0e060b060fe3a134eedb32
Jan Vorcak <vorcak@webscope.io>
2021-01-21 14:55:31 +0100

    Pass properties to Link in Alert component

--- packages/ui/__tests__/Alert.test.tsx
+++ packages/ui/__tests__/Alert.test.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 import { mountAndCheckA11Y } from '@hazelcast/test-helpers'
 
-import { AlertTriangle, CheckCircle, Info, Clipboard, AlertCircle } from 'react-feather'
-import { ToastType, ToastIconDescriptor } from '../src/Toast'
+import { AlertCircle, AlertTriangle, CheckCircle, Clipboard, Info } from 'react-feather'
+import { ToastIconDescriptor, ToastType } from '../src/Toast'
 import { Alert, AlertActionButton, AlertActionLink } from '../src/Alert'
 
 import styles from '../src/Alert.module.scss'
@@ -23,6 +23,8 @@ const AlertAction1: AlertActionButton = {
 const AlertAction2: AlertActionLink = {
   text: 'Link',
   href: '#',
+  target: '_blank',
+  rel: ['nofollow'],
 }
 
 describe('Alert', () => {
@@ -82,6 +84,27 @@ describe('Alert', () => {
     },
   )
 
+  it('Correct props are passed to an underlying Link Action component', async () => {
+    const wrapper = await mountAndCheckA11Y(<Alert type="success" title={title} content={content} actions={[AlertAction2]} />)
+
+    expect(wrapper.findDataTest('alert-actions').children().props()).toMatchObject({
+      href: AlertAction2.href,
+      target: AlertAction2.target,
+      rel: AlertAction2.rel,
+    })
+  })
+
+  it('Correct props are passed to an underlying Button Action component', async () => {
+    const wrapper = await mountAndCheckA11Y(<Alert type="success" title={title} content={content} actions={[AlertAction1]} />)
+
+    expect(wrapper.findDataTest('alert-actions').children().props()).toMatchObject({
+      children: AlertAction1.text,
+      onClick: AlertAction1.onClick,
+      iconLeft: AlertAction1.icon,
+      iconLeftAriaLabel: AlertAction1.ariaLabel,
+    })
+  })
+
   it('Close button calls closeToast prop handler', async () => {
     const closeToast = jest.fn()
 
@@ -97,19 +120,19 @@ describe('Alert', () => {
     expect(closeToast).toHaveBeenCalledTimes(1)
   })
 
-  const alertActionsTestData = [
+  const alertActionsTestData: Array<[string, Array<AlertActionLink | AlertActionButton>]> = [
     ['1 action', [AlertAction1]],
     ['2 actions', [AlertAction1, AlertAction2]],
   ]
 
-  it.each(alertActionsTestData)('Renders Alert with %s', async (_, actions) => {
-    const wrapper = await mountAndCheckA11Y(<Alert type="success" title={title} content={content} closeToast={noOp} />)
+  it.each(alertActionsTestData)('Renders Alert with %s', async (x, actions) => {
+    const wrapper = await mountAndCheckA11Y(<Alert type="success" title={title} content={content} closeToast={noOp} actions={actions} />)
 
     wrapper
       .findDataTest('alert-actions')
       .children()
       .forEach((child, cI) => {
-        expect(child.props()).toMatchObject(actions[cI])
+        expect(child.props()).toHaveProperty('children', actions[cI].text)
       })
   })
 
