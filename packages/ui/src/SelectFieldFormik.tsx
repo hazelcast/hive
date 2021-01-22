c03f139398c8847ef756e74feda48a3fb2b83761
utku <m.utku.k@gmail.com>
2021-01-22 22:13:52 +0300

    formik wrapper looks good. will consult someone to check it though.

--- packages/ui/src/SelectFieldFormik.tsx
+++ packages/ui/src/SelectFieldFormik.tsx
@@ -10,12 +10,26 @@ export type SelectFieldFormikProps<V extends object, OV = string> = SelectFieldE
     | {
         name: ExtractKeysOfValueType<V, SelectFieldOption<OV> | null>
         isClearable: true
+        isMulti?: false
         validate?: FieldValidatorGeneric<SelectFieldOption<OV> | null>
       }
     | {
         name: ExtractKeysOfValueType<V, SelectFieldOption<OV>>
         isClearable?: false
+        isMulti?: false
         validate?: FieldValidatorGeneric<SelectFieldOption<OV>>
+      }
+    | {
+        name: ExtractKeysOfValueType<V, SelectFieldOption<OV>[] | null>
+        isClearable: true
+        isMulti?: true
+        validate?: FieldValidatorGeneric<SelectFieldOption<OV>[] | null>
+      }
+    | {
+        name: ExtractKeysOfValueType<V, SelectFieldOption<OV>[]>
+        isClearable?: false
+        isMulti?: true
+        validate?: FieldValidatorGeneric<SelectFieldOption<OV>[]>
       }
   )
 
@@ -23,9 +37,11 @@ export const SelectFieldFormik = <V extends object, OV = string>({
   name,
   validate,
   isClearable,
+  isMulti,
   ...props
 }: SelectFieldFormikProps<V, OV>): ReactElement => {
-  type SelectedOption = typeof isClearable extends true ? SelectFieldOption<OV> | null : SelectFieldOption<OV>
+  type ValueType = typeof isMulti extends true ? SelectFieldOption<OV>[] : SelectFieldOption<OV>
+  type SelectedOption = typeof isClearable extends true ? ValueType | null : ValueType
 
   const [field, meta, { setValue, setTouched }] = useField<SelectedOption>({
     name,
@@ -38,6 +54,7 @@ export const SelectFieldFormik = <V extends object, OV = string>({
     value: field.value,
     onChange,
     isClearable,
+    isMulti,
   } as SelectFieldCoreDynamicProps<OV>
 
   return <SelectField {...props} {...dynamicProps} name={name} onBlur={field.onBlur} error={getFieldError(meta)} />
