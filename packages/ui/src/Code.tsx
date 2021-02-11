7575eabdb39c9da34781f7b0b1e82d9d8a011fac
utku <m.utku.k@gmail.com>
2021-02-12 01:31:08 +0300

    add missing languages and light theme

--- packages/ui/src/Code.tsx
+++ packages/ui/src/Code.tsx
@@ -12,22 +12,43 @@ import { PrismLight as SyntaxHighlighter } from 'react-syntax-highlighter'
 import jsx from 'react-syntax-highlighter/dist/esm/languages/prism/jsx'
 import cpp from 'react-syntax-highlighter/dist/esm/languages/prism/cpp'
 import csharp from 'react-syntax-highlighter/dist/esm/languages/prism/csharp'
+import go from 'react-syntax-highlighter/dist/esm/languages/prism/go'
 import java from 'react-syntax-highlighter/dist/esm/languages/prism/java'
 import kotlin from 'react-syntax-highlighter/dist/esm/languages/prism/kotlin'
+import python from 'react-syntax-highlighter/dist/esm/languages/prism/python'
+import scala from 'react-syntax-highlighter/dist/esm/languages/prism/scala'
 
 // Themes
+import vs from 'react-syntax-highlighter/dist/esm/styles/prism/vs'
 import darcula from 'react-syntax-highlighter/dist/esm/styles/prism/darcula'
 
 SyntaxHighlighter.registerLanguage('jsx', jsx)
 SyntaxHighlighter.registerLanguage('cpp', cpp)
 SyntaxHighlighter.registerLanguage('csharp', csharp)
+SyntaxHighlighter.registerLanguage('go', go)
 SyntaxHighlighter.registerLanguage('java', java)
 SyntaxHighlighter.registerLanguage('kotlin', kotlin)
+SyntaxHighlighter.registerLanguage('python', python)
+SyntaxHighlighter.registerLanguage('scala', scala)
 
-type language = 'jsx' | 'cpp' | 'csharp' | 'java' | 'kotlin'
+export type Language = 'jsx' | 'cpp' | 'csharp' | 'java' | 'kotlin' | 'go' | 'python' | 'scala'
 
-type CodeProps = {
-  language?: language
+export type Theme = 'light' | 'dark'
+
+type ThemeToStyleMapping = { [key in Theme]: unknown }
+
+const THEME_TO_STYLE_MAPPING: ThemeToStyleMapping = {
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  light: vs,
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+  dark: darcula,
+}
+
+export type CodeProps = {
+  language?: Language
+  theme?: Theme
+  showLineNumbers?: boolean
+  wrapLongLines?: boolean
   className?: string
 }
 
@@ -38,11 +59,20 @@ type CodeProps = {
   * react-syntax-highlighter itself is based on either one of these two popular js highlighters: Prism.js, highlightjs.org.
 
   * We're using the light version of the Prism backend.
+
+  * Only a select number of languages declared above are supported to keep it light.
 */
-export const Code: FC<CodeProps> = ({ language, children, className }) => {
+export const Code: FC<CodeProps> = ({ language, theme = 'light', showLineNumbers, wrapLongLines, children, className }) => {
+  const style: unknown = THEME_TO_STYLE_MAPPING[theme]
   return (
-    <div className={cn(styles.wrapper, className)}>
-      <SyntaxHighlighter className="xxx" language={language} style={darcula}>
+    <div className={cn(styles.container, className)}>
+      <SyntaxHighlighter
+        className={cn(styles.syntaxHighlighter)}
+        language={language}
+        showLineNumbers={showLineNumbers}
+        wrapLongLines={wrapLongLines}
+        style={style}
+      >
         {children}
       </SyntaxHighlighter>
     </div>
