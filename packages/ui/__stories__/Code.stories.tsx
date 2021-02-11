15447c5ac5e91ba0647fc76b390d3d318b823341
utku <m.utku.k@gmail.com>
2021-02-12 01:31:40 +0300

    add stories and tests

--- packages/ui/__stories__/Code.stories.tsx
+++ packages/ui/__stories__/Code.stories.tsx
@@ -1,8 +1,6 @@
 import React from 'react'
 import { Code } from '../src'
 
-// import styles from '../src/Code.module.scss'
-
 export default {
   title: 'Components/Code',
   component: Code,
@@ -20,4 +18,21 @@ for (let i = 1; i <= 100; i++) {
 
 export const Default = () => {
   return <Code language="jsx">{SAMPLE_CODE}</Code>
+}
+
+export const DarkWithLineNumbers = () => {
+  return (
+    <Code language="jsx" theme="dark" showLineNumbers={true}>
+      {SAMPLE_CODE}
+    </Code>
+  )
+}
+
+export const WrapLongLines = () => {
+  return (
+    <Code wrapLongLines>
+      Hazelcast IMDG clients and programming language APIs allow you to extend the benefits of operational in-memory computing to
+      applications in these languages. These clients and APIs (except Scala) are open source and supported by Hazelcast.
+    </Code>
+  )
 }
