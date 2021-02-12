aada07e7e25148bce640dbae3db16126f54dc774
Jan Koritak <koritakjan@gmail.com>
2021-02-12 09:49:32 +0100

    feat: wip

--- packages/ui/src/Calendar/CalendarTime.tsx
+++ packages/ui/src/Calendar/CalendarTime.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useMemo } from 'react'
+import React, { FC, useCallback, useMemo } from 'react'
 import { format } from 'date-fns'
 
 import { Button } from '../Button'
@@ -16,13 +16,34 @@ import styles from './CalendarTime.module.scss'
 export const CalendarTime: FC<any> = ({ date, value, onChange }) => {
   const datesSequence: string[] = useMemo(() => getDatesSequence(date).map((d) => format(d, 'hh:mm a')), [date])
 
+  const onChangeHandler = useCallback(
+    (e) => {
+      console.log(e.target.value)
+      onChange(e.target.value)
+    },
+    [onChange],
+  )
+
+  const onDateClickHandler = useCallback(
+    (dp) => () => {
+      console.log(dp)
+    },
+    [],
+  )
+
   return (
     <div className={styles.container}>
-      <div className={styles.header}>{value}</div>
+      <input className={styles.header} type="time" onChange={onChangeHandler} value={value} />
       <div className={styles.datePoints}>
         {datesSequence.map((dP) => (
           // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
-          <Button className={styles.datePoint} bodyClassName={styles.datePointBody} key={dP} kind="transparent" onClick={onChange}>
+          <Button
+            className={styles.datePoint}
+            bodyClassName={styles.datePointBody}
+            key={dP}
+            kind="transparent"
+            onClick={onDateClickHandler(dP)}
+          >
             {dP}
           </Button>
         ))}
