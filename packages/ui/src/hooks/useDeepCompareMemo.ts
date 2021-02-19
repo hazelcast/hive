eee6f66174bea6cff8a0b5369fda5fa8d8a25703
Andrey Goncharov <andrey@goncharov.page>
2021-02-19 15:29:04 +0300

    WIP

--- packages/ui/src/hooks/useDeepCompareMemo.ts
+++ packages/ui/src/hooks/useDeepCompareMemo.ts
@@ -4,5 +4,6 @@ import { useDeepEqual } from './useDeepEqual'
 
 export const useDeepCompareMemo = <T>(fn: () => T, deps: DependencyList) => {
   const depsMemoized = useDeepEqual(deps, [])
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   return useMemo(fn, depsMemoized)
 }
