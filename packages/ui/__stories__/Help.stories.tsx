38902892d1ee3f5e459be22261f46d8495a217b0
Andrey Goncharov <andrey@goncharov.page>
2021-03-04 21:29:30 +0300

    Add customizable sizes for Help (#202)

--- packages/ui/__stories__/Help.stories.tsx
+++ packages/ui/__stories__/Help.stories.tsx
@@ -7,10 +7,17 @@ export default {
   component: Help,
 }
 
+const helpText = 'Lorem Ipsum is simply dummy text of the printing and typesetting industry.'
+
 export const TextWithHelp = () => (
   <p>
     Lorem Ipsum is simply dummy text of the <span id="test">printing</span>
-    <Help parentId="test" helperText="Lorem Ipsum is simply dummy text of the printing and typesetting industry." />
+    <Help parentId="test" helperText={helpText} />
     and typesetting industry.
   </p>
 )
+
+export const HelpSmall = () => <Help parentId="test" helperText={helpText} />
+export const HelpNormal = () => <Help parentId="test" helperText={helpText} size="normal" />
+export const HelpLarge = () => <Help parentId="test" helperText={helpText} size="large" />
+export const HelpXLarge = () => <Help parentId="test" helperText={helpText} size="xlarge" />
