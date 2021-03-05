98374eedaad7db7577985a750837b4be98644d92
Jan Vorcak <vorcak@webscope.io>
2021-03-05 16:15:35 +0100

    Updated main package-lock.json

--- packages/ui/src/hooks/useDeepEqual.ts
+++ packages/ui/src/hooks/useDeepEqual.ts
@@ -4,6 +4,7 @@ import { dequal } from 'dequal'
 export const useDeepEqual = <T>(value: T, defaultValue: T) => {
   const ref = useRef(defaultValue)
 
+  // eslint-disable-next-line @typescript-eslint/no-unsafe-call
   if (!dequal(ref.current, value)) {
     ref.current = value
   }
