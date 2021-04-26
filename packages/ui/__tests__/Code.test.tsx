0cc740608b6795b5a73a7e306e4fb09f94849a04
utku <m.utku.k@gmail.com>
2021-04-27 01:50:01 +0300

    update unit tests

--- packages/ui/__tests__/Code.test.tsx
+++ packages/ui/__tests__/Code.test.tsx
@@ -3,15 +3,25 @@ import { mount } from 'enzyme'
 
 import { Code } from '../src/Code'
 
+const CODE = 'Hello World'
+
 describe('Code', () => {
   it('Renders the default Code correctly', () => {
-    const wrapper = mount(<Code value="Hello World" />)
+    const wrapper = mount(<Code value={CODE} />)
 
-    // following does not work because of how CodeMirror creates itself
+    // following does not work because of how CodeMirror creates itself (appends to DOM dynamically)
     // expect(wrapper.find('.cm-editor')).toHaveLength(1)
+    //
+    // there's a problem with enzyme's .find with dynamic DOM appending:
+    // https://github.com/enzymejs/enzyme/issues/1233
+    //
+    // so here's a trick that resolves it:
+    const renderedWrapper = wrapper.render()
 
-    // so we just check the html contents.
-    expect(wrapper.html()).toContain('cm-editor')
-    expect(wrapper.html()).toContain('cm-wrap')
+    expect(renderedWrapper.find('.cm-editor')).toHaveLength(1)
+    expect(renderedWrapper.find('.cm-wrap')).toHaveLength(1)
+
+    // .html works as expected; does not need .render
+    expect(wrapper.html()).toContain(CODE)
   })
 })
--- packages/ui/__tests__/CodeEditor.test.tsx
+++ packages/ui/__tests__/CodeEditor.test.tsx
@@ -3,15 +3,25 @@ import { mount } from 'enzyme'
 
 import { CodeEditor } from '../src/CodeEditor'
 
+const CODE = 'Hello World'
+
 describe('Code', () => {
   it('Renders the default CodeEditor correctly', () => {
-    const wrapper = mount(<CodeEditor value="Hello World" />)
+    const wrapper = mount(<CodeEditor value={CODE} />)
 
-    // following does not work because of how CodeMirror creates itself
+    // following does not work because of how CodeMirror creates itself (appends to DOM dynamically)
     // expect(wrapper.find('.cm-editor')).toHaveLength(1)
+    //
+    // there's a problem with enzyme's .find with dynamic DOM appending:
+    // https://github.com/enzymejs/enzyme/issues/1233
+    //
+    // so here's a trick that resolves it:
+    const renderedWrapper = wrapper.render()
 
-    // so we just check the html contents.
-    expect(wrapper.html()).toContain('cm-editor')
-    expect(wrapper.html()).toContain('cm-wrap')
+    expect(renderedWrapper.find('.cm-editor')).toHaveLength(1)
+    expect(renderedWrapper.find('.cm-wrap')).toHaveLength(1)
+
+    // .html works as expected; does not need .render
+    expect(wrapper.html()).toContain(CODE)
   })
 })
