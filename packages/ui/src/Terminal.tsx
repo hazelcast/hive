3877c775e8f25a884d18601da27b18d0a815b125
Andrey Goncharov <andrey@goncharov.page>
2021-04-27 12:49:25 +0300

    Implement Terminal

---
+++ packages/ui/src/Terminal.tsx
@@ -0,0 +1,193 @@
+import React, { FC, Fragment, KeyboardEventHandler, memo, useRef, useState } from 'react'
+import { DataTestProp } from '@hazelcast/helpers'
+import cn from 'classnames'
+import { useUID } from 'react-uid'
+import { Terminal as TerminalIcon } from 'react-feather'
+
+import styles from './Terminal.module.scss'
+import { Icon } from './Icon'
+
+export class CommandTimeoutError extends Error {
+  name = 'CommandTimeoutError'
+}
+
+const HISTORY_LIMIT = 100
+const COMMAND_TIMEOUT = 60000
+
+export type TerminalProps = {
+  onExecute: (command: string, args: string[]) => Promise<string[]>
+  commandTimeout?: number
+  historyLimit?: number
+  className?: string
+  inputClassName?: string
+  welcomeMessage?: string
+  initialExecutionHistory?: ExecutionHistoryState[]
+  initialCommand?: string
+  initialLoading?: boolean
+} & DataTestProp
+
+export interface ExecutionHistoryState {
+  command: string
+  res?: string[]
+  error?: string
+}
+
+export const Terminal: FC<TerminalProps> = memo(
+  ({
+    'data-test': dataTest,
+    onExecute,
+    commandTimeout = COMMAND_TIMEOUT,
+    historyLimit = HISTORY_LIMIT,
+    className,
+    inputClassName,
+    welcomeMessage,
+    initialExecutionHistory = [],
+    initialCommand = '',
+    initialLoading = false,
+  }) => {
+    const [inputVal, setInputVal] = useState(initialCommand)
+
+    const [executionHistoryPointer, setExecutionHistoryPointer] = useState<number | undefined>()
+    const [executionHistory, setExecutionHistory] = useState<ExecutionHistoryState[]>(initialExecutionHistory)
+
+    const addExecutionHistoryItem = (newHistoryItem: ExecutionHistoryState) => {
+      setExecutionHistory((prevHistory) => {
+        const newHistory = [...prevHistory, newHistoryItem]
+
+        if (newHistory.length > historyLimit) {
+          newHistory.shift()
+        }
+
+        return newHistory
+      })
+      setExecutionHistoryPointer(undefined)
+    }
+
+    const [executing, setExecuting] = useState(initialLoading)
+
+    const inputRef = useRef<HTMLInputElement>(null)
+
+    const execute = async () => {
+      const commandTokens = inputVal.split(' ')
+
+      let timer!: NodeJS.Timeout
+
+      try {
+        setExecuting(true)
+
+        const res = await Promise.race<string[]>([
+          onExecute(commandTokens[0], commandTokens.slice(1)),
+          // This promise will never be resolved so we can type is as we want to make TS happy
+          new Promise<string[]>((_, reject) => {
+            timer = setTimeout(() => {
+              reject(new CommandTimeoutError(`${inputVal} exceeded timeout ${commandTimeout}`))
+            }, commandTimeout)
+          }),
+        ])
+
+        addExecutionHistoryItem({ command: inputVal, res })
+      } catch (err) {
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment
+        const errorTyped: Error = err
+        addExecutionHistoryItem({ command: inputVal, error: errorTyped.message })
+      } finally {
+        setExecuting(false)
+        clearTimeout(timer)
+        setInputVal('')
+        // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+        inputRef.current!.focus()
+      }
+    }
+
+    const traverseHistory = (direction: 'back' | 'forward') => {
+      let nextHistoryPointer: number | undefined
+
+      if (direction === 'back') {
+        nextHistoryPointer = executionHistoryPointer ? executionHistoryPointer - 1 : executionHistory.length - 1
+      } else {
+        nextHistoryPointer = executionHistoryPointer ? executionHistoryPointer + 1 : 0
+      }
+
+      const nextHistoryItem = nextHistoryPointer ? executionHistory[nextHistoryPointer] : undefined
+
+      if (!nextHistoryItem) {
+        nextHistoryPointer = undefined
+      }
+
+      setExecutionHistoryPointer(nextHistoryPointer)
+
+      setInputVal(nextHistoryItem?.command ?? '')
+    }
+
+    const onKeyPress: KeyboardEventHandler = (e) => {
+      switch (e.key) {
+        case 'Enter': {
+          void execute()
+          break
+        }
+        case 'Up': // IE/Edge specific value
+        case 'ArrowUp': {
+          traverseHistory('back')
+          break
+        }
+        case 'Down': // IE/Edge specific value
+        case 'ArrowDown': {
+          traverseHistory('forward')
+          break
+        }
+      }
+    }
+
+    const loadingTextId = useUID()
+
+    return (
+      <div data-test={dataTest} className={cn(styles.container, className)}>
+        {welcomeMessage && <p className={styles.welcome}>{welcomeMessage}</p>}
+        <div className={styles.history} role="alert">
+          {executionHistory.map((historyItem, i) => (
+            <Fragment key={i}>
+              <p className={styles.input}>
+                <Icon icon={TerminalIcon} ariaHidden className={styles.terminalIcon} />
+                <input type="text" value={historyItem.command} readOnly tabIndex={-1} className={styles.readonly} />
+              </p>
+              {historyItem.res && (
+                <p className={styles.commandRes}>
+                  {historyItem.res.map((resString, i) => (
+                    <p key={i}>{resString}</p>
+                  ))}
+                </p>
+              )}
+              {historyItem.error && <p className={styles.commandError}>{historyItem.error}</p>}
+            </Fragment>
+          ))}
+        </div>
+        <div className={styles.currentCommand}>
+          <div className={styles.input}>
+            <Icon icon={TerminalIcon} ariaHidden className={styles.terminalIcon} />
+            <input
+              type="text"
+              value={inputVal}
+              onChange={(e) => setInputVal(e.target.value)}
+              onKeyPress={onKeyPress}
+              aria-describedby={executing ? loadingTextId : undefined}
+              disabled={executing}
+              autoFocus
+              ref={inputRef}
+              className={inputClassName}
+            />
+          </div>
+          {executing && (
+            <div className={styles.loading}>
+              <span className={styles.loadingA11yText} id={loadingTextId}>
+                Command is executing. Please, wait.
+              </span>
+              ...
+            </div>
+          )}
+        </div>
+      </div>
+    )
+  },
+)
+
+Terminal.displayName = 'Terminal'
