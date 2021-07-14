0dbe32277da60078a1b1484d6ad0bff464394874
danad02 <polansky.dan@gmail.com>
2021-07-14 09:24:02 +0200

    Refactor: change Card's margin bearers, update Card stories (#347)
    * refactor: change Card's margin bearers, update Card stories
    
    * test: fix tests
    
    * fix: add space to title only if heading content is present

--- packages/ui/__stories__/Card.stories.tsx
+++ packages/ui/__stories__/Card.stories.tsx
@@ -1,34 +1,47 @@
 import React from 'react'
 import { Database } from 'react-feather'
+import { Meta, Story } from '@storybook/react'
 
-import { Card, IconButton } from '../src'
+import { Card, CardProps, IconButton } from '../src'
 
-import utilityStyles from './utils.scss'
 import styles from './Card.stories.module.scss'
-
-const cardContent =
-  'Hogshead topsail draft careen mizzen fluke gaff cog aye Buccaneer. To go on account topgallant Jolly Roger maroon overhaul ho landlubber or just lubber prow pillage clap of thunder. Holystone jack black jack sloop bowsprit Sea Legs matey aft fluke brigantine.'
 
 export default {
   title: 'Components/Card',
   component: Card,
-}
-
-export const Simple = () => <Card title="Card title">{cardContent}</Card>
-
-Simple.parameters = {
-  design: {
-    type: 'figma',
-    url: 'https://www.figma.com/file/8mVm6LTbp2Z0RaWWjTZoft/%F0%9F%90%9DHIVE-Hazelcast-Design-System?node-id=10093%3A8',
+  parameters: {
+    design: {
+      type: 'figma',
+      url: 'https://www.figma.com/file/8mVm6LTbp2Z0RaWWjTZoft/%F0%9F%90%9DHIVE-Hazelcast-Design-System?node-id=492%3A67',
+    },
   },
+  args: {
+    title: 'Card title',
+    children:
+      'Hogshead topsail draft careen mizzen fluke gaff cog aye Buccaneer. To go on account topgallant Jolly Roger maroon overhaul ho landlubber or just lubber prow pillage clap of thunder. Holystone jack black jack sloop bowsprit Sea Legs matey aft fluke brigantine.',
+  },
+} as Meta<CardProps>
+
+const Template: Story<CardProps> = (args) => {
+  return <Card {...args} />
 }
 
-export const WithSeparator = () => (
-  <Card title="Card title" separator>
-    {cardContent}
-  </Card>
-)
+export const Simple = Template.bind({})
 
+export const WithoutTitle = Template.bind({})
+WithoutTitle.args = {
+  title: undefined,
+}
+
+export const WithoutContent = Template.bind({})
+WithoutContent.args = {
+  children: undefined,
+}
+
+export const WithSeparator = Template.bind({})
+WithSeparator.args = {
+  separator: true,
+}
 WithSeparator.parameters = {
   design: {
     type: 'figma',
@@ -36,12 +49,10 @@ WithSeparator.parameters = {
   },
 }
 
-export const WithIcon = () => (
-  <Card title="Card title" headingIcon={Database}>
-    {cardContent}
-  </Card>
-)
-
+export const WithIcon = Template.bind({})
+WithIcon.args = {
+  headingIcon: Database,
+}
 WithIcon.parameters = {
   design: {
     type: 'figma',
@@ -49,31 +60,20 @@ WithIcon.parameters = {
   },
 }
 
-export const WithIconAndSeparator = () => (
-  <Card title="Card title" headingIcon={Database} separator>
-    {cardContent}
-  </Card>
-)
+export const WithIconAndSeparator = Template.bind({})
+WithIconAndSeparator.args = {
+  ...WithSeparator.args,
+  ...WithIcon.args,
+}
 
-export const WithSmallText = () => (
-  <Card title="Card title">
-    <div className={utilityStyles.typographyBodySmall}>{cardContent}</div>
-  </Card>
-)
+export const WithHeadingContent = Template.bind({})
+WithHeadingContent.args = {
+  headingContent: <IconButton kind="primary" ariaLabel="Check out the Database" icon={Database} component="a" href="#" />,
+}
 
-export const WithHeadingContent = () => (
-  <Card
-    title="Card title"
-    headingContent={<IconButton kind="primary" ariaLabel="Check out the Database" icon={Database} component="a" href="#" />}
-  >
-    {cardContent}
-  </Card>
-)
-
-export const WithCustomStyling = () => (
-  <Card title="Card title" headingIcon={Database} iconClassName={styles.customIconClass} titleClassName={styles.customTitleClass}>
-    {cardContent}
-  </Card>
-)
-
-export const WithoutTitle = () => <Card>{cardContent}</Card>
+export const WithCustomStyling = Template.bind({})
+WithCustomStyling.args = {
+  ...WithIcon.args,
+  iconClassName: styles.customIconClass,
+  titleClassName: styles.customTitleClass,
+}
