fe83e0f469b4d134990ea1f0b72c6bd50351cc71
Serhii Frolov <allfayn@gmail.com>
2022-07-12 16:59:20 +0800

    update(code): add simple default format date/time function

--- packages/services/src/time.ts
+++ packages/services/src/time.ts
@@ -3,4 +3,20 @@ export class TimeService {
   public static currentTimestamp() {
     return Date.now()
   }
+
+  public static format(source: Date): string {
+    return `${TimeService.formatDate(source)} ${TimeService.formatTime(source)}`
+  }
+
+  public static formatTime(source: Date): string {
+    return `${('0' + source.getHours().toString()).slice(-2)}:${('0' + source.getMinutes().toString()).slice(-2)}:${(
+      '0' + source.getSeconds().toString()
+    ).slice(-2)}`
+  }
+
+  public static formatDate(source: Date): string {
+    return `${('0' + source.getDate().toString()).slice(-2)}-${('0' + (source.getMonth() + 1).toString()).slice(
+      -2,
+    )}-${source.getFullYear()}`
+  }
 }
