50f0ac7388148e0ba4ee7f3260e224b2ddbd5ed3
Serhii Frolov <allfayn@gmail.com>
2022-07-13 19:14:38 +0800

    update(code): add simple default format date/time function (#472)
    * update(code): add simple default format date/time function
    
    * fix(code): date format
    
    * fix(code): add default format template value

--- packages/services/src/time.ts
+++ packages/services/src/time.ts
@@ -1,6 +1,24 @@
 // Useful to override current time for our code in tests, leaving third-party code untouched.
 export class TimeService {
+  public static DEFAULT_FORMAT = 'YYYY-MM-DD hh:mm:ss'
+
   public static currentTimestamp() {
     return Date.now()
+  }
+
+  public static format(source: Date): string {
+    return `${TimeService.formatDate(source)} ${TimeService.formatTime(source)}`
+  }
+
+  public static formatTime(source: Date): string {
+    return `${('0' + source.getHours().toString()).slice(-2)}:${('0' + source.getMinutes().toString()).slice(-2)}:${(
+      '0' + source.getSeconds().toString()
+    ).slice(-2)}`
+  }
+
+  public static formatDate(source: Date): string {
+    return `${source.getFullYear()}-${('0' + (source.getMonth() + 1).toString()).slice(-2)}-${('0' + source.getDate().toString()).slice(
+      -2,
+    )}`
   }
 }
