dfd3f4bfcc349a6ed3609afca1aa2ab1dc24b01e
Mykola Fant <mykola.fant@hazelcast.com>
2022-07-28 11:03:48 +0300

    rotate cluster icon (#473)
    * update(ui): rotate cluster icon
    
    * update(ui): rotate cluster icon
    
    * update(ui): rotate cluster icon

--- packages/icons/src/Cluster.tsx
+++ packages/icons/src/Cluster.tsx
@@ -5,15 +5,15 @@ import { HazelcastIcon } from './types'
 export const Cluster: HazelcastIcon = ({ color = 'currentColor', size = 24, ...rest }) => (
   <svg width={size} height={size} viewBox="0 0 30 30" fill="none" xmlns="http://www.w3.org/2000/svg" {...rest}>
     <path d="M12.5 3.75H3.75V12.5H12.5V3.75Z" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
+    <path d="M26.25 3.75H17.5V12.5H26.25V3.75Z" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
     <path
-      d="M26.25 3.75H17.5V12.5H26.25V3.75Z"
+      d="M26.25 17.5H17.5V26.25H26.25V17.5Z"
       fill={color}
       stroke={color}
       strokeWidth="1.5"
       strokeLinecap="round"
       strokeLinejoin="round"
     />
-    <path d="M26.25 17.5H17.5V26.25H26.25V17.5Z" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
     <path d="M12.5 17.5H3.75V26.25H12.5V17.5Z" stroke={color} strokeWidth="1.5" strokeLinecap="round" strokeLinejoin="round" />
   </svg>
 )
