be26268d7fdfdb22224becd6771d19bb9f384fd4
Mykola Fant <mykola.fant@hazelcast.com>
2023-01-04 12:56:33 +0200

    style intro page

--- packages/ui/docs/MdxDocsPage.tsx
+++ packages/ui/docs/MdxDocsPage.tsx
@@ -9,6 +9,8 @@ export const MdxDocsPage: React.FC = ({ children }) => (
       right: 0,
       bottom: 0,
       left: 0,
+      background: '#F2F5F9',
+      overflow: 'auto',
     }}
   >
     {children}
