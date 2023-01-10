5502437d8ec1c7bdbbb072a3ea9a1fb639ada738
Mykola Fant <mykola.fant@hazelcast.com>
2023-01-10 12:35:44 +0200

    number badge className (#488)

--- packages/ui/src/NumberBadge.tsx
+++ packages/ui/src/NumberBadge.tsx
@@ -1,16 +1,18 @@
 import React, { FC } from 'react'
 
 import styles from './NumberBadge.module.scss'
+import cn from 'classnames'
 
 export type NumberBadgeProps = {
   number: number
+  className?: string
 }
 
-export const NumberBadge: FC<NumberBadgeProps> = ({ number }) => {
+export const NumberBadge: FC<NumberBadgeProps> = ({ number, className }) => {
   const formattedNumber: string = number <= 99 ? number.toString() : '+99'
 
   return (
-    <div data-test="number-badge" className={styles.numberBadge}>
+    <div data-test="number-badge" className={cn(styles.numberBadge, className)}>
       {formattedNumber}
     </div>
   )
