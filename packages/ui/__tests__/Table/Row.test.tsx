cd418506da7926b14732eb7e70bccc0ac5e8e8e2
Ahmet Berat Can <32953408+aberatcan@users.noreply.github.com>
2023-05-05 17:37:00 +0300

    better accessibility score with ARIA updates (#493)
    * update aria-label of SelectField

--- packages/ui/__tests__/Table/Row.test.tsx
+++ packages/ui/__tests__/Table/Row.test.tsx
@@ -58,10 +58,10 @@ describe('Row', () => {
         className: styles.linkRow,
         role: undefined,
         'aria-rowindex': undefined,
-
+        'aria-owns': '1',
         children: expect.anything(),
       },
-      { className: styles.link, style: undefined, href: 'testHref', children: 'Row' },
+      { className: styles.link, id: '1', style: undefined, href: 'testHref', children: 'Row' },
     ],
     [
       { ariaRowIndex: 1, href: 'testHref', style: { width: 40 }, className: 'testClassName', role: '', children: 'Row' },
@@ -70,10 +70,10 @@ describe('Row', () => {
         className: `${styles.linkRow} testClassName`,
         role: '',
         'aria-rowindex': 1,
-
+        'aria-owns': '2',
         children: expect.anything(),
       },
-      { className: styles.link, style: { width: 40 }, href: 'testHref', children: 'Row' },
+      { className: styles.link, id: '2', style: { width: 40 }, href: 'testHref', children: 'Row' },
     ],
   ]
 
