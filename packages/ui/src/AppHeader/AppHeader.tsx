4ad602cd6bb4e4141e79faa960b25b5f1bbc1816
Yevhen Martynenko <yevhen.martynenko@hazelcast.com>
2024-09-09 12:20:42 +0300

    *fix testing library not in dev dependencies

--- packages/ui/src/AppHeader/AppHeader.tsx
+++ packages/ui/src/AppHeader/AppHeader.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import cls from 'classnames'
+import { DataTestProp } from '@hazelcast/helpers'
 
 import { EnvironmentBadge } from './EnvironmentBadge'
 import { AppHeaderMenu, AppHeaderMenuProps } from './AppHeaderMenu'
@@ -8,7 +9,7 @@ import { SelectCluster, SelectClusterProps } from './SelectCluster'
 
 import styles from './AppHeader.module.scss'
 
-export interface AppHeaderProps {
+export interface AppHeaderProps extends DataTestProp {
   name: ReactNode
   className?: string
   children?: ReactNode
@@ -18,8 +19,17 @@ export interface AppHeaderProps {
   menuContent?: AppHeaderMenuProps['children']
 }
 
-export const AppHeader = ({ name, className, children, environment, clusterSelectProps, logoProps, menuContent }: AppHeaderProps) => (
-  <header className={cls(styles.root, className)}>
+export const AppHeader = ({
+  name,
+  className,
+  children,
+  environment,
+  clusterSelectProps,
+  logoProps,
+  menuContent,
+  'data-test': dataTest = 'app-header',
+}: AppHeaderProps) => (
+  <header data-test={dataTest} className={cls(styles.root, className)}>
     <AppHeaderLogo {...logoProps} />
     <span className={styles.name}>{name}</span>
     {clusterSelectProps && <SelectCluster {...clusterSelectProps} />}
