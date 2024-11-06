ed0607752182207aa920c9e9c9a0bf68dbf45819
Yevhen Martynenko <yevhen.martynenko@hazelcast.com>
2024-11-06 14:21:27 +0200

    *feat renderAndCheckA11Y for testing-library

--- packages/test-helpers/src/render.ts
+++ packages/test-helpers/src/render.ts
@@ -2,14 +2,23 @@ import { ReactElement } from 'react'
 import { render, RenderResult } from '@testing-library/react'
 import { axe, toHaveNoViolations } from 'jest-axe'
 
-import { axeDefaultOptions } from './axe'
+import { axeDefaultOptions, AxeOptions } from './axe'
 
 expect.extend(toHaveNoViolations)
 
-export const renderAndCheckA11Y = async <P>(node: ReactElement<P>): Promise<RenderResult> => {
+export interface RenderAndCheckA11YOptions {
+  axeOptions?: AxeOptions
+}
+
+export const renderAndCheckA11Y = async <P>(
+  node: ReactElement<P>,
+  { axeOptions }: RenderAndCheckA11YOptions = {
+    axeOptions: axeDefaultOptions,
+  },
+): Promise<RenderResult> => {
   const result = render(node)
 
-  const results = await axe(result.container, axeDefaultOptions)
+  const results = await axe(result.container, axeOptions)
   expect(results).toHaveNoViolations()
 
   return result
